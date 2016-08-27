c8d5c77fabc3ef8b5c89fae7dd002ccdc1202436
k0pernicus <antonin.carette@gmail.com>
2016-08-27 00:33:41 -0400

    Add 2 commands: add and crawl

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 	"os/user"
 
 	"github.com/k0pernicus/giwyn/giwyn"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	app   = kingpin.New("GOwin", "A command-line app to follow your git repositories")
+	add   = app.Command("add", "Add the current directory to the list of git repositories")
+	crawl = app.Command("crawl", "Crawl the entire hard drive to find and save git repositories")
 )
 
 func main() {
@@ -15,17 +22,26 @@ func main() {
 	*/
 	giwyn.InitTraces(os.Stderr, os.Stdout, os.Stdout)
 
-	/*
-		Get current user, in order to retrieve his home directory path
-	*/
-	userHomeDirectory, err := user.Current()
-	if err != nil {
-		panic(fmt.Sprintf("ERROR: Canno't retrieve the user home directory, due to %s", err))
-	}
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case add.FullCommand():
+		pwd, err := os.Getwd()
+		if err != nil {
+			panic(fmt.Sprintf("ERROR: Canno't retrieve the user current path, due to %s", err))
+		}
+		fmt.Println("PWD: ", pwd)
 
-	/*
-		Get each git path - currently (demo), we don't use them...
-	*/
-	giwyn.FindGitObjects(userHomeDirectory.HomeDir)
+	case crawl.FullCommand():
+		/*
+			Get current user, in order to retrieve his home directory path
+		*/
+		userHomeDirectory, err := user.Current()
+		if err != nil {
+			panic(fmt.Sprintf("ERROR: Canno't retrieve the user home directory, due to %s", err))
+		}
+		/*
+			Get each git path - currently (demo), we don't use them...
+		*/
+		giwyn.FindGitObjects(userHomeDirectory.HomeDir)
+	}
 
 }
