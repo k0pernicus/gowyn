564d07eda0eb1bdcc76d2f9f4ee8879877f15015
k0pernicus <antonin.carette@gmail.com>
2016-08-28 10:35:26 -0400

    New main - add crawl as a flag for add

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/user"
+	/*"os/user"*/
 
 	"github.com/k0pernicus/gowyn/gowyn"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	app   = kingpin.New("GOwin", "A command-line app to follow your git repositories")
+	app = kingpin.New("GOwin", "A command-line app to follow your git repositories")
+	/*ADD*/
 	add   = app.Command("add", "Add the current directory to the list of git repositories")
-	crawl = app.Command("crawl", "Crawl the entire hard drive to find and save git repositories")
-	rm    = app.Command("rm", "Remove the current directory to the list followed git repositories")
+	crawl = add.Flag("crawl", "Crawl to add git repositories found since the current directory").Bool()
+	/*RM*/
+	rm = app.Command("rm", "Remove the current directory to the list followed git repositories")
 )
 
 func main() {
@@ -28,24 +30,24 @@ func main() {
 		panic(fmt.Sprintf("ERROR: Canno't retrieve the user current path, due to \"%s\"", err))
 	}
 
-	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
-	case add.FullCommand():
-		if err := gowyn.GetGitObject(pwd); err != nil {
-			panic(fmt.Sprintf("ERROR: Canno't get the git object from %s, due to \"%s\"", pwd, err))
-		}
-
-	case crawl.FullCommand():
-		/*
-			Get current user, in order to retrieve his home directory path
-		*/
+	/*
 		userHomeDirectory, err := user.Current()
 		if err != nil {
 			panic(fmt.Sprintf("ERROR: Canno't retrieve the user home directory, due to \"%s\"", err))
 		}
-		/*
-			Get each git path - currently (demo), we don't use them...
-		*/
-		gowyn.FindGitObjects(userHomeDirectory.HomeDir)
+	*/
+
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case add.FullCommand():
+		if *crawl {
+			if err := gowyn.FindGitObjects(pwd); err != nil {
+				panic(fmt.Sprintf("ERROR: Crawl to find git paths failed, due to \"%s\"", err))
+			}
+		} else {
+			if err := gowyn.GetGitObject(pwd); err != nil {
+				panic(fmt.Sprintf("ERROR: Canno't get the git object from %s, due to \"%s\"", pwd, err))
+			}
+		}
 
 	case rm.FullCommand():
 		if err := gowyn.RmGitObject(pwd); err != nil {
