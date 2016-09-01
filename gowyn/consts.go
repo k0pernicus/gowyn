9d7628139ceb2672c36b245cbeee78565fe2afd6
k0pernicus <antonin.carette@gmail.com>
2016-09-01 22:10:38 +0200

    Add .gitignore name file

--- gowyn/consts.go
+++ gowyn/consts.go
@@ -2,9 +2,10 @@ package gowyn
 
 const (
 	/*FILE NAME*/
-	GIT_NAME_DIR    = ".git"
-	GOWYN_NAME_FILE = ".gowyn"
-	GOWYN_NAME_CONF = ".gowyn_conf"
+	GIT_NAME_DIR        = ".git"
+	GITIGNORE_NAME_FILE = ".gitignore"
+	GOWYN_NAME_FILE     = ".gowyn"
+	GOWYN_NAME_CONF     = ".gowyn_conf"
 	/*.GOWYN FILE*/
 	UPDATED_S = "UPDATED = "
 	/*.GOWYN CONF*/
